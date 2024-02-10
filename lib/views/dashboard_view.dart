1dd4572b26f1326c8d505e473a715512e0e9e37e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-10 02:28:48 +0200

    Show CustomDrawer in DashboardMobileLayout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_adaptive/core/widgets/responsive_layout.dart';
+import 'package:responsive_adaptive/widgets/custom_drawer.dart';
+import 'package:responsive_adaptive/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_adaptive/widgets/dashboard_tablet_layout.dart';
 import 'package:responsive_adaptive/widgets/desktop_dashboard_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xffF7F9FA),
+              leading: IconButton(
+                onPressed: () {
+                  _scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(Icons.menu),
+                color: Colors.black,
+              ),
+            )
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
       body: ResponsiveLayout(
-        mobileLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const Padding(
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+          child: DashboardMobileLayout(),
+        ),
         tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DesktopDashboardLayout(),
       ),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
     );
   }
 }
